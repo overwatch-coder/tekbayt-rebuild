17dcf545d1afae337bb43c937a2aa834fc50ed5e
Nyamadi Atsu <devbrainy98@gmail.com>
2024-02-02 16:56:20 +0100

    fixed location not found error on success page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,60 +1,76 @@
 "use client";
 import Button from "@/components/shared/Button";
-import { useRouter, useSearchParams } from "next/navigation";
+import { redirect, useRouter, useSearchParams } from "next/navigation";
 import React, { Suspense, useEffect, useState } from "react";
 
-const Success = () => {
+type SuccessMessageProps = {
+  setMessage: React.Dispatch<React.SetStateAction<string | null>>;
+  message: string | null;
+};
+
+function SuccessMessage({ message, setMessage }: SuccessMessageProps) {
   const searchParams = useSearchParams();
-  const router = useRouter();
-  const [message, setMessage] = useState(searchParams.get("success"));
 
   useEffect(() => {
-    if (!searchParams.has("success")) {
-      setMessage(null);
-      router.push("/contact-us");
-    }
+    setMessage(
+      searchParams.has("success") ? searchParams.get("success") : null
+    );
+  }, [searchParams, setMessage]);
 
-    if (searchParams.get("success") !== "true") {
+  return (
+    <section
+      data-aos="zoom-out"
+      className="px-5 md:px-20 flex items-center justify-center py-10 md:py-20"
+    >
+      <div className="flex flex-col gap-8 md:max-w-xl w-full rounded shadow-md py-10 px-5 items-center">
+        <h2 className="text-3xl md:text-4xl text-center font-semibold text-black-100 animate-bounce">
+          {message === "true"
+            ? "Thank you for contacting us!"
+            : "Failed to send message!"}
+        </h2>
+
+        <p className="text-base md:text-lg text-center text-black-80 tracking-wide">
+          {message === "true"
+            ? "We will be in touch with you shortly!"
+            : "Please try again later"}
+        </p>
+
+        <Button
+          name={message === "true" ? "Submit Another" : "Try Again"}
+          url="/contact-us"
+          className="bg-orange-100 w-fit text-center"
+        />
+      </div>
+    </section>
+  );
+}
+
+const SuccessPage = () => {
+  const [message, setMessage] = useState<string | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (message !== "true") {
       setTimeout(() => {
-        router.push("/contact-us");
+        setMessage(null);
+        return redirect("/contact-us");
       }, 5000);
     }
-  }, [router, searchParams]);
+  }, [message, router]);
 
   if (!message) {
-    return router.push("/contact-us");
+    return redirect("/contact-us");
   }
 
   return (
-    <Suspense>
+    <>
       <title>Thank you for contacting us! | Tekabyt</title>
       <meta name="description" content="We will be in touch with you shortly" />
-      <section
-        data-aos="zoom-out"
-        className="px-5 md:px-20 flex items-center justify-center py-10 md:py-20"
-      >
-        <div className="flex flex-col gap-8 md:max-w-xl w-full rounded shadow-md py-10 px-5 items-center">
-          <h2 className="text-3xl md:text-4xl text-center font-semibold text-black-100 animate-bounce">
-            {message === "true"
-              ? "Thank you for contacting us!"
-              : "Failed to send message!"}
-          </h2>
-
-          <p className="text-base md:text-lg text-center text-black-80 tracking-wide">
-            {message === "true"
-              ? "We will be in touch with you shortly!"
-              : "Please try again later"}
-          </p>
-
-          <Button
-            name={message === "true" ? "Submit Another" : "Try Again"}
-            url="/contact-us"
-            className="bg-orange-100 w-fit text-center"
-          />
-        </div>
-      </section>
-    </Suspense>
+      <Suspense>
+        <SuccessMessage message={message} setMessage={setMessage} />
+      </Suspense>
+    </>
   );
 };
 
-export default Success;
+export default SuccessPage;
