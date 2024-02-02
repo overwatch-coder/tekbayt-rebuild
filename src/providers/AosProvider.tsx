b2b9e6e2a77f51a3fcd5f4ea2265954210b2772f
Nyamadi Atsu <devbrainy98@gmail.com>
2024-02-02 17:17:59 +0100

    changed aos init params

--- src/providers/AosProvider.tsx
+++ src/providers/AosProvider.tsx
@@ -12,8 +12,8 @@ const AosProvider = ({ children }: AosProviderProps) => {
   useEffect(() => {
     // initialising library
     Aos.init({
-      delay: 50, // values from 0 to 3000, with step 50ms
-      duration: 700, // values from 0 to 3000, with step 50ms
+      delay: 10, // values from 0 to 3000, with step 50ms
+      duration: 500, // values from 0 to 3000, with step 50ms
       easing: "ease-in-out", // default easing for AOS animations
       once: true, // whether animation should happen only once
       mirror: false, // whether elements should animate out while scrolling past them
