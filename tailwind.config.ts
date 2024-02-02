a7425a15dddea1c62ada32db79ee088d09e6143b
Nyamadi Atsu <devbrainy98@gmail.com>
2024-02-02 17:14:40 +0100

    changed marquee animaton duration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
       animation: {
         fade: 'fade 2s ease-in-out 1s infinite alternate',
         change: 'change 2s ease-in-out 1s infinite alternate',
-        marquee: 'marquee 40s linear 0s infinite reverse',
+        marquee: 'marquee 30s ease-in-out 0s infinite reverse',
       },
       content: {
         image: 'url("/assets/hero_curve_inactive.png")',
